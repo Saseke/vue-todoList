08eb555f27f41e58071d920bc0e36d994133157b
Yoke-Yoke <717964921@qq.com>
2018-04-10 09:23:32 +0800

    springboot打包发布

--- todoList-web/src/main/java/com/yoke/todoList/web/TodoListApplication.java
+++ todoList-web/src/main/java/com/yoke/todoList/web/TodoListApplication.java
@@ -3,6 +3,9 @@ package com.yoke.todoList.web;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.context.annotation.Bean;
 
 /**
  * @author Yoke
@@ -11,6 +14,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication(scanBasePackages = "com.yoke")
 @MapperScan("com.yoke.todoList.mapper")
 public class TodoListApplication {
+    @Bean
+    public EmbeddedServletContainerFactory servletContainer() {
+
+        return new TomcatEmbeddedServletContainerFactory();
+
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(TodoListApplication.class, args);
     }
